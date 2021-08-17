0708da14a1f948f98759e5a3325e6bf8092c02a3
Mwiczkowski <miloszwiczkowski@gmail.com>
2021-08-17 18:52:35 +0200

    MySuperBank app course finished

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyBankyStuff;
+using System;
+using Humanizer;
 
 namespace MySuperBank
 {
@@ -6,40 +8,20 @@ namespace MySuperBank
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Kendra", 10000);
+             var account = new BankAccount("Kendra", 10000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} for {account.Balance}");
 
-            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
-            Console.WriteLine(account.Balance);
-
+            
             account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
-            Console.WriteLine(account.Balance);
+            account.MakeWithdrawal(5, DateTime.Now, "Coffee");
+            account.MakeWithdrawal(5, DateTime.Now, "Diet Coke");
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            account.MakeWithdrawal(7, DateTime.Now, "Tea");
+            account.MakeWithdrawal(8, DateTime.Now, "Pants");
 
             Console.WriteLine(account.GetAccountHistory());
 
-            //// Test for a negative balance.
-            //try
-            //{
-            //    account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw");
-            //}
-            //catch (InvalidOperationException e)
-            //{
-            //    Console.WriteLine("Exception caught trying to overdraw");
-            //    Console.WriteLine(e.ToString());
-            //}
-
-            //// Test that the initial balances must be positive.
-            //BankAccount invalidAccount;
-            //try
-            //{
-            //    invalidAccount = new BankAccount("invalid", -55);
-            //}
-            //catch (ArgumentOutOfRangeException e)
-            //{
-            //    Console.WriteLine("Exception caught creating account with negative balance");
-            //    Console.WriteLine(e.ToString());
-            //    return;
-            //}
+           
         }
     }
 }
